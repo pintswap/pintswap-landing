37867cb8b37a9606372c2f4153eba86cb90712e5
yoyobojo <angellolazar@gmail.com>
2023-04-13 17:58:37 -0400

    fixed type

--- src/ui/base/logo.tsx
+++ src/ui/base/logo.tsx
@@ -1,14 +1,10 @@
 type ILogoProps = {
-  xl?: boolean;
+  textSize?: `text-${string}`;
 };
 
 const Logo = (props: ILogoProps) => {
-  const fontStyle = props.xl
-    ? 'font-semibold text-3xl'
-    : 'font-semibold text-xl';
-
   return (
-    <h2 className="text-3xl">
+    <h2 className={props.textSize ? props.textSize : 'text-3xl'}>
         <span className="text-pink-500">Pint</span>
         <span className="text-sky-400">Swap</span>
     </h2>
