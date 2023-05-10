a8292525f018e4b18de2695ae8dbd50ca4141b4c
yoyobojo <angellolazar@gmail.com>
2023-05-10 00:52:20 -0400

    added 2 animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import { Base } from '../ui/base';
 import { Hero } from '../ui/views/hero';
 
-const Index = () => {
+const Index = () => (
   <Base>
-    <Hero />
-  </Base>;
-};
+    <div className="z-50 relative">
+      <Hero />
+    </div>
+    {/* <ParticlesContainer /> */}
+  </Base>
+);
 
 export default Index;
