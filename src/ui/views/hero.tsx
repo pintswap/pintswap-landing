103cc9d394e78f72e6555ce3f3f622915b93ed65
yoyobojo <angellolazar@gmail.com>
2023-05-16 14:38:19 -0400

    added dripping gif

--- src/ui/views/hero.tsx
+++ src/ui/views/hero.tsx
@@ -1,25 +1,33 @@
 import Link from 'next/link';
 import { Padding } from '../layouts/padding';
 import { Button } from '../components/button';
-import { Logo } from '../base/logo';
 
 const Hero = () => {
   return (
-    <div className="h-screen flex justify-center items-center md:justify-between md:px-8 lg:px-16 xl:px-24">
+    <div
+      className="h-screen flex justify-center items-center md:px-8 lg:px-16 xl:px-24"
+      style={{
+        backgroundImage: 'url("/assets/pintswap-dripping.gif")',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'contain',
+        paddingTop: '4rem',
+      }}
+    >
       <Padding type="x">
-        <div className="flex flex-col items-center justify-center gap-8 text-center md:text-left md:items-start mb-12">
-          <div className="max-w-xl md:max-w-full flex flex-col justify-center items-center md:items-start">
-            <img
-              src="/logo/ps-logo.png"
-              alt="PintSwap Logo"
-              className="w-28 h-28 md:w-20 md:h-20 lg:w-24 lg:h-24"
-            />
-            <Logo tag="h1" textSize="text-4xl lg:text-5xl" />
-            <h2 className="text-lg text-gray-400">
-              Peer-to-Peer token swaps
-              <br className="block" /> using multi-party transaction scripts.
-            </h2>
-          </div>
+        <div className="flex justify-center relative -top-16 md:-top-28 lg:-top-36">
+          <img
+            src="/logo/ps-logo.png"
+            alt="PintSwap Logo"
+            className="w-20 h-20 md:w-24 md:h-24"
+          />
+        </div>
+
+        <div className="flex flex-col items-center justify-center gap-4 text-center relative top-16 lg:top-24">
+          <h2 className="md:text-lg text-gray-400">
+            Peer-to-Peer token swaps
+            <br className="block" /> using multi-party transaction scripts.
+          </h2>
 
           <ul className="navbar flex flex-col gap-3 items-center font-medium md:gap-4 md:flex-row">
             <li>
