0704aa9b065ddf392e304a71e7b797d108d1a748
yoyobojo <angellolazar@gmail.com>
2023-07-22 01:04:02 -0400

    landing done - missing new animation

---
+++ src/ui/components/animated-brand.tsx
@@ -0,0 +1,24 @@
+export const AnimatedBrand = ({
+  size,
+  subtitle,
+}: {
+  size: string;
+  subtitle?: string;
+}) => (
+  <div className="flex flex-col text-center justify-center w-full">
+    {subtitle && (
+      <h2 className="uppercase text-lg relative top-20 md:top-36">
+        {subtitle}
+      </h2>
+    )}
+    <div
+      style={{
+        backgroundImage: 'url("/assets/pintswap-dripping.gif")',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'contain',
+        height: size,
+      }}
+    />
+  </div>
+);
---
+++ src/ui/components/social-list.tsx
@@ -0,0 +1,66 @@
+import Link from 'next/link';
+import { FaDiscord, FaTwitter } from 'react-icons/fa';
+
+export const SocialList = ({
+  direction = 'vertical',
+  size = '30px',
+  absolute,
+}: {
+  direction?: 'vertical' | 'horizontal';
+  className?: string;
+  size?: `${string}px`;
+  absolute?: 'right-center' | 'bottom-right' | 'left-center' | 'bottom-left';
+}) => {
+  const directionClass = direction === 'vertical' ? 'flex-col' : 'flex-row';
+  const locatedAt = () => {
+    const baseClass = `absolute`;
+    let customClass = '';
+    switch (absolute) {
+      case 'bottom-left': {
+        customClass = 'bottom-4 left-4';
+        break;
+      }
+      case 'bottom-right': {
+        customClass = 'bottom-4 right-4';
+        break;
+      }
+      case 'left-center': {
+        customClass = 'bottom-1/2 left-4';
+        break;
+      }
+      default: {
+        customClass = 'bottom-1/2 right-4';
+        break;
+      }
+    }
+    return `${baseClass} ${customClass}`;
+  };
+  return (
+    <div className={locatedAt()}>
+      <div
+        className={`flex ${directionClass} items-center justify-center gap-4 md:gap-6`}
+      >
+        <button className="transition duration-200 group relative z-10 cursor-pointer">
+          <Link href="" target="_blank">
+            <a>
+              <FaDiscord
+                size={`${size}`}
+                className="group-hover:fill-pink-400 transition duration-200"
+              />
+            </a>
+          </Link>
+        </button>
+        <button className="transition duration-200 group relative z-10 cursor-pointer">
+          <Link href="https:" target="_blank">
+            <a>
+              <FaTwitter
+                size={`${size}`}
+                className="group-hover:fill-pink-400 transition duration-200"
+              />
+            </a>
+          </Link>
+        </button>
+      </div>
+    </div>
+  );
+};
