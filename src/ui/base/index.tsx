c91656952c4fdb9a342b60d841ff78dc4b68e84b
yoyobojo <angellolazar@gmail.com>
2023-07-26 13:20:02 -0400

    added padding for other pages

--- src/ui/base/index.tsx
+++ src/ui/base/index.tsx
@@ -3,19 +3,25 @@ import { ReactNode } from 'react';
 import { AppConfig } from '../../utils/app-config';
 import { Meta } from './meta';
 import { Navbar } from './navbar';
+import { useRouter } from 'next/router';
 
 type IBaseProps = {
   children?: ReactNode;
   nav?: boolean;
 };
 
-const Base = (props: IBaseProps) => (
-  <div className="antialiased">
-    <Meta title={AppConfig.title} description={AppConfig.description} />
-    {props.nav && <Navbar />}
-    {props.children}
-    {/* <Footer /> */}
-  </div>
-);
+const Base = (props: IBaseProps) => {
+  const { pathname } = useRouter();
+  return (
+    <div className="antialiased">
+      <Meta title={AppConfig.title} description={AppConfig.description} />
+      {props.nav && <Navbar />}
+      <div className={pathname !== '/' ? 'pb-4 sm:pb-0' : ''}>
+        {props.children}
+      </div>
+      {/* <Footer /> */}
+    </div>
+  );
+};
 
 export { Base };
